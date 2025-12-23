5120dd2f549e0338408fce69ab65cb4e641a9f0e
Erwin Wessels <erwin@pleo.io>
2025-12-23 22:43:47 +0100

    Day 7 part 2-incorrect

--- day07/src/main/java/Main.java
+++ day07/src/main/java/Main.java
@@ -60,9 +60,69 @@ void part1(Stream<String> doc) {
     }
     println("Splits: "+splits);
 }
-
 void part2(Stream<String> doc) {
-    println("Part 2");
+    println("Part 1");
+
+    List<List<Field>> space =
+            doc.map(
+                    l -> l.chars()
+                            .mapToObj(c ->
+                                    switch (c) {
+                                        case '.' -> Field.FREE;
+                                        case '^' -> Field.SPLITTER;
+                                        case 'S' -> Field.START;
+                                        default -> throw new RuntimeException("Unexpected character " + c);
+                                    }
+                            )
+                            .toList()
+            ).toList();
+
+    int startX = -1;
+    List<Field> spaceFirst = space.getFirst();
+    for (int i = 0; i < spaceFirst.size(); i++) {
+        if (spaceFirst.get(i) == Field.START) {
+            startX = i;
+            break;
+        }
+    }
+
+    println("Start X: " + startX);
+    int rowCount = space.size();
+    int colCount = space.getFirst().size();
+    Map<Integer, Integer> beams = new HashMap<>();
+    beams.put(startX, 1);
+
+    for (int y=1; y<rowCount; y++) {
+        List<Integer> keySet = new ArrayList<>(beams.keySet());
+        for (int x: keySet) {
+            int pathCount = beams.get(x);
+            switch (space.get(y).get(x)) {
+                case Field.FREE -> {
+                    // nothing to do
+                }
+                case Field.SPLITTER -> {
+                    if (x>0) {
+                        if (beams.containsKey(x-1)) {
+                            beams.put(x-1, beams.get(x-1) + pathCount);
+                        } else {
+                            beams.put(x-1, pathCount);
+                        }
+                    }
+                    if (x<colCount-1) {
+                        if (beams.containsKey(x+1)) {
+                            beams.put(x+1, beams.get(x+1) + pathCount);
+                        } else {
+                            beams.put(x+1, pathCount);
+                        }
+                    }
+                    beams.remove(x);
+                }
+
+            }
+        }
+    }
+    int pathCount = beams.values().stream().mapToInt(i -> i).sum();
+    println("Path count: "+pathCount);
 }
 
 void withInput(String inputType,
