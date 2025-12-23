ffbc1a3fef0703208b16bb83356cb9c85af86616
Erwin Wessels <erwin@pleo.io>
2025-12-23 17:59:09 +0100

    Day 5 part 2

--- day05/src/main/java/Main.java
+++ day05/src/main/java/Main.java
@@ -1,18 +1,45 @@
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 import static java.lang.IO.println;
-import static java.lang.Integer.parseInt;
 import static java.lang.Long.parseLong;
 
 static class LongPair {
-    long a;
-    long b;
+    long start;
+    long finish;
 
-    LongPair(long a, long b) {
-        this.a = a;
-        this.b = b;
+    LongPair(long start, long finish) {
+        this.start = start;
+        this.finish = finish;
     }
 
     Boolean contains(long x) {
-        return a <= x && x <= b;
+        return start <= x && x <= finish;
+    }
+
+    Boolean updateIfOverlaps(long otherStart, long otherFinish) {
+        if (otherStart > finish) {
+            return false;
+        }
+        if (otherFinish < start) {
+            return false;
+        }
+        println("Overlaps with " + toDisplayString()+": "+otherStart + "|" + otherFinish);
+        if (otherStart < start) {
+            start = otherStart;
+        }
+        if (otherFinish > finish) {
+            finish = otherFinish;
+        }
+        return true;
+    }
+
+    long size() {
+        return finish - start + 1L;
+    }
+
+    String toDisplayString() {
+        return start + "-" + finish;
     }
 }
 
@@ -41,12 +68,59 @@ void part1(Stream<String> doc) {
             freshRanges.add(pair);
         }
 
-    };
+    }
+
     println("Fresh count: " + freshCount);
 }
 
 void part2(Stream<String> doc) {
     println("Part 2");
+    ArrayList<LongPair> freshRanges = new ArrayList<>();
+    for (String line : (Iterable<String>) doc::iterator) {
+        if (line.isEmpty()) {
+            break;
+        } else {
+            println("> " + line);
+            String[] parts = line.split("-", 2);
+            long start = parseLong(parts[0]);
+            long finish = parseLong(parts[1]);
+            boolean anyOverlaps =
+                    freshRanges.stream().anyMatch(r -> r.updateIfOverlaps(start, finish));
+            if (!anyOverlaps) {
+                LongPair pair =
+                        new LongPair(
+                                start,
+                                finish
+                        );
+                freshRanges.add(pair);
+            }
+        }
+    }
+    println("\n--- Overview");
+    freshRanges.forEach( r -> println(r.toDisplayString()));
+    println("\n--- Cleaning");
+    // now we need to merge any that overlap anyway
+    ArrayList<LongPair> toRemoveRanges = new ArrayList<>();
+    for (int iterTarget = 1; iterTarget < freshRanges.size(); iterTarget++) {
+        LongPair rTarget = freshRanges.get(iterTarget);
+        println("Checking rTarget " + rTarget.toDisplayString());
+        for (int iterCandidate = 0; iterCandidate < iterTarget; iterCandidate++) {
+            LongPair rCandidate  = freshRanges.get(iterCandidate);
+//            println("Candidate " + rCandidate.toDisplayString());
+            if (rCandidate.updateIfOverlaps(rTarget.start, rTarget.finish)) {
+                println("Overlapped; scheduling to remove target " + rTarget.toDisplayString());
+                toRemoveRanges.add(rTarget);
+                break;
+            }
+        }
+    }
+
+    println("To remove " + toRemoveRanges.size());
+    freshRanges.removeAll(toRemoveRanges);
+    println("\nResult:");
+    freshRanges.forEach( r -> println(r.toDisplayString() + ": " + r.size()));
+    long ingredientCount = freshRanges.stream().mapToLong(LongPair::size).sum();
+    println("Ingredient count: " + ingredientCount);
 }
 
 void withInput(String inputType,
